65f23257e1afbe3fab4e5b8837c053e1982ecff8
Hasanraza25 <hasansaeed2526@gmail.com>
2025-08-11 19:30:19 +0500

    changes

--- backend/config/dbc.php
+++ backend/config/dbc.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'pickles');
+define('DB_USER', 'u523339939_devpickles');
+define('DB_PASS', 'CybertronLabs@2026');
+define('DB_NAME', 'u523339939_devpickles');
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 ?>
